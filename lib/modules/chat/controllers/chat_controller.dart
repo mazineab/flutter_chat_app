2dbaa6aa82b7f2be90c0ec3357d95d99d85c088f
mazineab <mazinabjlal@gmail.com>
2024-12-28 14:52:01 +0000

    Fix issue of duplicate conversations and notification sending

--- lib/modules/chat/controllers/chat_controller.dart
+++ lib/modules/chat/controllers/chat_controller.dart
@@ -3,13 +3,16 @@ import 'dart:async';
 import 'package:chat_app/data/models/conversation.dart';
 import 'package:chat_app/data/models/message.dart';
 import 'package:chat_app/data/repositories/chat_repositorie.dart';
+import 'package:chat_app/data/repositories/users_repositorie.dart';
 import 'package:chat_app/modules/current_user_controller.dart';
+import 'package:chat_app/utils/services/notification_service.dart';
 import 'package:chat_app/widget/snackBars/snack_bars.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ChatController extends GetxController{
+  final UsersRepositorie _usersRepositories=Get.put(UsersRepositorie());
   final ChatRepositories _chatRepositories=Get.put(ChatRepositories());
   CurrentUserController currentUserController=Get.find();
   ScrollController scrollController=ScrollController();
@@ -67,8 +70,22 @@ class ChatController extends GetxController{
         isRead: false,
         createdAt: Timestamp.fromDate(DateTime.now()),
       );
-      await _chatRepositories.sendMessage(message, conversation.value.uid!,currentUserController.authUser.value.docId==conversation.value.senderDocId);
+      bool isSender=currentUserController.authUser.value.docId==conversation.value.senderDocId;
+      await _chatRepositories.sendMessage(message, conversation.value.uid!,isSender);
       textEditingController.clear();
+      String? fcmToken=await _usersRepositories.getUserFcmToken(isSender
+          ?conversation.value.receiverDocId ?? ''
+          :conversation.value.senderDocId ?? ''
+      );
+      if(fcmToken!=null){
+        await Get.find<NotificationService>().sendNotifications(
+            fcmToken: fcmToken,
+            title: "Chat App",
+            body: "${currentUserController.authUser.value.name} send you message",
+            userId: ""
+        );
+      }
+
     }catch(e){
       CustomSnackBar.showError("Failed to send your message, please try again.");
       Exception(e);
